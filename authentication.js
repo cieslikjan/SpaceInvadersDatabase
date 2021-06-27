82da383a863e91d534de5720bb2697b78abeb2a2
jaice66 <jan.cieslik@stud.hs-ruhrwest.de>
2021-06-27 23:43:00 +0200

    connection to db

--- authentication.js
+++ authentication.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 mongoose.connect("mongodb+srv://testUser:test123@cluster0.xngl3.mongodb.net/userDB", { useNewUrlParser: true, useUnifiedTopology:true });
 
@@ -11,48 +13,62 @@ mongoose.connection.on('error', (err) => {
 mongoose.connection.on('connected', () => console.log('Data Db connected'));   
 
 const app =  express();
+app.use(cors());
 const port = 8080;
 app.use(express.json({extended:false}));
 
 const UserSchema = new mongoose.Schema({
     username:String,
-    password:String
+    password:String,
+    email:String
 });
+
 const User = mongoose.model("User", UserSchema);
 app.listen(port, () => {
     console.log('Authentication service started on port 8080');
 });
 
-// app.post("/", (req, res) => {
-//     const user = new User(req.body);
-//     user.save()
-//       .then(item => {
-//         res.send("item saved to database");
-//       })
-//       .catch(err => {
-//         res.status(400).send("unable to save to database");
-//       });
-//   });
-
-
-
-//TODO: define salt
- //salting
-
-app.post('/', (req,res) => {
-    const accessTokenSecret = '123';
-    //Query database for user
-    const user = new User(req.body);
-    User.findOne({username: user.username}, (err, obj) => console.log(obj));
+//TODO: Check if username already exists in db.
+//TODO: Check if email is already registered. 
+app.post('/validateUsername', (req,res) => {
     
-    if(User){   
-        const accessStoken = jwt.sign({username : User.username}, accessTokenSecret);
-        res.json({
-            accessStoken
+
+});
+
+app.post('/register', (req, res) => {
+    let password = req.body.password;
+
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            req.body.password = hash;
+            const user = new User(req.body);
+            user.save()
+              .then(user => {
+                res.send("user saved to database");
+              })
+              .catch(err => {
+                res.status(400).send("unable to save to database");
+              });
         });
-    }else{
-        res.send('Username or password incorrect');
-    }
+      });
+});
+
+
+app.post('/login', (req,res) => {
+    const accessTokenSecret = '123';
+
+    User.findOne({username: req.body.username}, (err, obj) => {
+        bcrypt.compare(req.body.password, obj.password, function(err, result) {
+            if (result) {
+                const accessStoken = jwt.sign({username : User.username}, accessTokenSecret);
+                res.json({
+                    accessStoken
+                });
+            } else {
+                res.send('Username or password incorrect');
+            }
+          });
+    });
     
 });
 
