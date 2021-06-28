fc2a209030a1b1ab2193fbcccad67314f0f87840
jaice66 <jan.cieslik@stud.hs-ruhrwest.de>
2021-06-28 16:42:14 +0200

    added jwtauth, token, refreshtoken,logout

--- authentication.js
+++ authentication.js
@@ -4,6 +4,12 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const dayjs = require('dayjs');
+const cookieParser = require('cookie-parser');
+
+// TODO: Generate on startup
+const accessTokenSecret = '35gxQ2Mj7W3_H9P=HPAk.Mr?2M4.cu23;pqZ3.t]nFFv_{U)?)EUBwL}y%Fi=gZb';
+const refreshTokenSecret = '123';
+var refreshTokens = [];
 
 mongoose.connect("mongodb+srv://testUser:test123@cluster0.xngl3.mongodb.net/userDB", { useNewUrlParser: true, useUnifiedTopology:true });
 
@@ -15,27 +21,56 @@ mongoose.connection.on('connected', () => console.log('Data Db connected'));
 
 const app =  express();
 app.use(cors());
+app.use(cookieParser());
+
 const port = 8080;
 app.use(express.json({extended:false}));
 
 const UserSchema = new mongoose.Schema({
     username:String,
     password:String,
-    email:String
+    email:String,
+    rank:Number,
 });
 
 const User = mongoose.model("User", UserSchema);
 app.listen(port, () => {
-    console.log('Authentication service started on port 8080');
+    console.log('Authentication service started on port ' + port);
 });
 
-app.post('/register', (req, res) => {
-    let findUsername = User.findOne({username: req.body.username});
+const authJWT = (req, res, next) => {
+    let token = req.cookies.token;
+    let rtoken = req.cookies.rtoken;
+
+    if (!refreshTokens.includes(rtoken)) {
+        return res.sendStatus(403);
+    }
+    
+    if (token) {
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        })
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+app.get('/rank', authJWT, (req, res) => {
+    res.send(req.user);
+});
+
+app.post('/register', async (req, res) => {
+    let findUsername = await User.findOne({username: req.body.username}).exec();
     if (findUsername !== null) {
         return res.status(400).send({success: false, msg: "Username nicht einzigartig"});
     }
     
-    let findEmail = User.findOne({email: req.body.email});
+    let findEmail = await User.findOne({email: req.body.email}).exec();
     if (findEmail !== null) {
         return res.status(400).send({success: false, msg: "Email nicht einzigartig"});
     }
@@ -44,8 +79,9 @@ app.post('/register', (req, res) => {
 
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(password, salt, function(err, hash) {
-            req.body.password = hash;
-            new User(req.body).save().then(user => {
+            let user = {username: req.body.username, password: hash, email: req.body.email, rank: 0};
+
+            new User(user).save().then(user => {
                 return res.status(200).send({success: true, msg: "user saved to database"});
             }).catch((err) => {
                 return res.status(400).send({success: false, msg: "user not to database"});
@@ -56,22 +92,58 @@ app.post('/register', (req, res) => {
 
 
 app.post('/login', (req,res) => {
-    const accessTokenSecret = '123';
-
     User.findOne({username: req.body.username}, (err, obj) => {
         bcrypt.compare(req.body.password, obj.password, function(err, result) {
             if (result) {
-                const accessStoken = jwt.sign({username : User.username}, accessTokenSecret);
-                res.cookie('token', accessStoken, {
+                const accessToken = jwt.sign({username : req.body.username}, accessTokenSecret, {expiresIn: '20m'});
+                const refreshToken = jwt.sign({username : req.body.username}, refreshTokenSecret);
+
+                res.cookie('token', accessToken, {
                     httpOnly: true,
                     expires: dayjs().add(20, 'minutes').toDate()
                 })
-                return res.status(200).end();
+
+                res.cookie('rtoken', refreshToken, {
+                    httpOnly: true,
+                })
+
+                return res.status(200).send({success: true, msg: "Logged in!"});
             } else {
                 return res.status(400).send('Username or password incorrect');
             }
         });
     });
-    
+});
+
+app.post('/token', (req, res) => {
+    let rtoken = req.cookies.rtoken;
+
+    if (!rtoken) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(rtoken)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.cookie('token', accessToken, {
+            httpOnly: true,
+            expires: dayjs().add(20, 'minutes').toDate()
+        })
+    });
+});
+
+app.post('/logout', (req, res) => {
+    let rtoken = req.cookies.rtoken;
+    refreshTokens = refreshTokens.filter(t => t !== rtoken);
+
+    res.send({success: true, msg: "Logout successful"});
 });
 
